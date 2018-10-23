65c1ec5dd7349c4b470e84b2d243703740648b68
Valto Silva <valtosilva7@gmail.com>
2018-10-23 19:41:49 -0400

    First commit - Ready to start the project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'front-friends';
+  // tslint:disable-next-line:quotemark
+  title = "Friends's Circle";
 }
